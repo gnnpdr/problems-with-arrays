00f71f23b984144bb8f0e3930719300c882918c5
markova-anna <markova.anna@phystech.edu>
2024-08-27 13:29:30 +0300

    moving elements and assertion in action

--- main.cpp
+++ main.cpp
@@ -1,20 +1,26 @@
 #include <stdio.h>
+#include <assert.h>
 
-
-void print_every_element (int array[], size_t size);
+void move_elements (int array[], size_t size);
 
 int main()
 {
     int array[] = {1, 2, 3, 4, 5};
     const size_t size = sizeof(array)/sizeof(array[0]); 
 
-    print_every_element (array, size);  //use the pointer to the array and need amount of elements
+    move_elements (array, size);
 
     return 0;  
 }
 
-void print_every_element (int array[], size_t size)
+void move_elements (int array[], size_t size)
 {
     for (size_t i = 0; i < size; i++)
+    {
+        assert (i   < size);
+        assert (i+1 < size);  //brings asser to the final element of array
+
+        array[i] = array[i+1];
         printf("%d\n", array[i]);
+    }
 }
