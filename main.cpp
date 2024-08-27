4d3995ccb6a40ad736cc10d114cf516fd62c6634
markova-anna <markova.anna@phystech.edu>
2024-08-27 17:11:21 +0300

    angle array

--- main.cpp
+++ main.cpp
@@ -1,31 +1,33 @@
 #include <stdio.h>
 
-int printmas (int intmas[][3], size_t amount_of_str, size_t amount_of_elements_in_str); 
+int printmas (int* intmas, size_t amount_of_str, size_t amount_of_elements_in_str); 
 
 int main ()
 {
 	int ar[][3] = 
         {
         {1, 2, 3},
-        {4, 5, 6}
+        {4, 5, 6},
+        {7, 8, 9}
         };
 
     const size_t amount_of_str = (sizeof(ar)/sizeof(ar[0]));
     const size_t amount_of_eliments_in_str  = sizeof(ar[0])/sizeof(ar[0][0]); 
 
-    printmas(ar, amount_of_str, amount_of_eliments_in_str);      
+    printmas(*ar, amount_of_str, amount_of_eliments_in_str);      
 }
 
-int printmas (int intmas[][3], size_t amount_of_str, size_t amount_of_elements_in_str) 
+int printmas (int* intmas, size_t amount_of_str, size_t amount_of_elements_in_str) 
 {
    for(size_t str = 0; str < amount_of_str; str++)  
    {
         for(size_t el = 0; el < amount_of_elements_in_str; el++) 
         {
-            intmas[str][el] = intmas[str][el+1];
-            printf("%d ", intmas[str][el]);
+            if (el < str)
+            {
+                printf("%d ", *(intmas + str*amount_of_elements_in_str + el));
+            }
         }
         printf("\n");
    } 
-    
 }
