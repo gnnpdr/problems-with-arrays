50c3baf441213e613e8061de5009d9308b88d9f7
markova-anna <markova.anna@phystech.edu>
2024-08-27 19:47:52 +0300

    add assert and const

--- main.cpp
+++ main.cpp
@@ -9,17 +9,17 @@ struct TriangAr
 
 void enter_size (TriangAr* the_table);
 
-void initial_struct (TriangAr* the_table);
-void find_capacity (int size, int* capacity); 
+void initial_struct (TriangAr* the_table, int *const capacity);
+void find_capacity (const int size, int *const capacity); 
 
 void full_out_el (TriangAr* the_table);
-void find_el(int row, int col, int* els);
+void find_el(const int row, const int col, int *const els);
 
 void print_el (TriangAr* the_table);
 
 void clean (TriangAr* the_table);
 
-void print_ar(TriangAr* the_table, int capacity);
+void print_ar(TriangAr* the_table, const int capacity);
 
 int main ()
 {
@@ -29,17 +29,21 @@ int main ()
 
     enter_size(&the_table);
 
-    initial_struct(&the_table); 
+    initial_struct(&the_table, &capacity); 
 }
 
 
 void enter_size(TriangAr* the_table)
 {
+    assert (the_table != nullptr);
     scanf("%d", &the_table->size);
 }
 
-void initial_struct (TriangAr* the_table, int* capacity)
+void initial_struct (TriangAr* the_table, int *const capacity)
 {
+    assert (the_table != nullptr);
+    assert (capacity != nullptr);
+
     find_capacity(the_table->size, capacity);
  
     the_table->result = (int *)calloc(capacity, sizeof(int));
@@ -51,14 +55,19 @@ void initial_struct (TriangAr* the_table, int* capacity)
     }
 }
 
-void find_capacity (int size, int* capacity)  //ar pr
+void find_capacity (const int size, int *const capacity)  //ar pr
 {
+    assert (size != NULL);
+    assert (capacity != nullptr);
+
     for(int i = 0; i <= size; i++)
         *capacity += i;
 }
 
 void full_out_el (TriangAr* the_table)
 {
+    assert (the_table != nullptr);
+
     size_t row = 0, col = 0;
     scanf("%d", &row);
     scanf("%d", &col);
@@ -69,14 +78,20 @@ void full_out_el (TriangAr* the_table)
     scanf("%d", &(the_table->result) + els);
 }
 
-find_el(int row, int col, int* els)
+void find_el(const int row, const int col, int *const els)
 {
+    assert (row != NULL);
+    assert (col != NULL);
+    assert (els != nullptr);
+
     find_capacity(row-1, els);
     *els += col;
 }
 
 void print_el (TriangAr* the_table)
 {
+    assert (the_table != nullptr);
+
     size_t row = 0, col = 0;
     scanf("%d", &row);
     scanf("%d", &col);
@@ -89,11 +104,16 @@ void print_el (TriangAr* the_table)
 
 void clean (TriangAr* the_table)
 {
+    assert (the_table != nullptr);
+
     free(the_table->result);
 }
 
-void print_ar(TriangAr* the_table, int capacity)  //here capacity was found
+void print_ar(TriangAr* the_table, const int capacity)  //here capacity was found
 {
+    assert (the_table != nullptr);
+    assert (capacity != nullptr);
+
     int k = 0;
 
     for (int row = 0; row < the_table->size; row++)
